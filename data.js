ce9bb67ba87c7d1428add0f275c4fcfebcf38e0c
Brittany  Arsi <brittmacarsi@icloud.com>
2017-09-10 15:10:22 -0400

    correct fetch call, correct the 301 error code to redirect http

--- data.js
+++ data.js
@@ -1,3 +1,4 @@
+console.log("connected");
 fetch('http://m.lowes.com/CatalogServices/product/nvalue/v1_0?nValue=4294857975&maxResults=6&showURL=1&rollUpVariants=1&showUrl=true&storeNumber=0595&priceFlag=rangeBalance&showMarketingBullets=1')
 .then(
   function(response){
@@ -9,14 +10,14 @@ fetch('http://m.lowes.com/CatalogServices/product/nvalue/v1_0?nValue=4294857975&
       console.log(data.results);
       let products = data.results;
 
-      function renderProducts(){
-        return `
-          ${products.map(product =>
-            <div class="product">
-              <ul>
-          )}`
-
-      }
+      // function renderProducts(){
+      //   return `
+      //     ${products.map(product =>
+      //       <div class="product">
+      //         <ul>
+      //     )}`
+      //
+      // }
     })
   }
 )
--- index.js
+++ index.js
@@ -1,6 +1,45 @@
 const express = require('express');
 const app = express();
 const mustacheExpress = require('mustache-express');
+var fetch = require('node-fetch');
+var http = require('follow-redirects').http;
+
+// fetch request from http.. redirect allowed
+http.get("http://m.lowes.com/CatalogServices/product/nvalue/v1_0?nValue=4294857975&maxResults=6&showURL=1&rollUpVariants=1&showUrl=true&storeNumber=0595&priceFlag=rangeBalance&showMarketingBullets=1", (res) => {
+  const { statusCode } = res;
+  const contentType = res.headers['content-type'];
+
+  let error;
+  if (statusCode !== 200) {
+    error = new Error('Request Failed.\n' +
+                      `Status Code: ${statusCode}`);
+  } else if (!/^application\/json/.test(contentType)) {
+    error = new Error('Invalid content-type.\n' +
+                      `Expected application/json but received ${contentType}`);
+  }
+  if (error) {
+    console.error(error.message);
+    // consume response data to free up memory
+    res.resume();
+    return;
+  }
+
+  res.setEncoding('utf8');
+  let rawData = '';
+  res.on('data', (chunk) => { rawData += chunk; });
+  res.on('end', () => {
+    try {
+      const parsedData = JSON.parse(rawData);
+      console.log(parsedData); // showing the data raw-pretty
+    } catch (e) {
+      console.error(e.message);
+    }
+  });
+}).on('error', (e) => {
+  console.error(`Got error: ${e.message}`);
+});
+
+    fetch.Promise = require('bluebird');
  app.engine('mustache', mustacheExpress());
  app.set('view engine', 'mustache');
  app.set('views', './views');
